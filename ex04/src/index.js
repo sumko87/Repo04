2e48bcc64e2519f593d18895a9c5ea9c969eee86
Sumedin Bilalic <sumko87@gmail.com>
2022-03-31 23:11:31 +0200

    fixing scientific notation in JS

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 function main(){
     //Only change code below this line
     var myDecimal = 8.2;
-    var myDecimalE = 0.00254;
+    var myDecimalE = 2.54e-3;
     //Only change code above this line
     return {
         myDecimal,
